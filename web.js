dff43d105a5f4168fc08205d9bcd2e4596b7ab5d
Pamela Clunes <pamela.reikochan@gmail.com>
2013-07-10 03:02:26 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+
+var buffer = fs.readFileSync('index.html','utf-8');
+var str = buffer.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
